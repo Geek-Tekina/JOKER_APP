4d06d0b4900593967cd9878b1fb647e515d5e55b
Geek-Tekina <tpbc1717@gmail.com>
2024-04-12 11:41:01 +0530

    Changes in Db file and env

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-export const dbConnection =()=>{
-    mongoose.connect(process.env.MONGO_URI, {
-        dbName: "Joker_APP",
-    })
-    .then(()=>{
+export const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "Joker_APP", // Change to "JOKER_APP" as per your database name
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true, // Optionally, enable useCreateIndex to avoid deprecation warning
+            useFindAndModify: false // Optionally, disable useFindAndModify to avoid deprecation warning
+        });
         console.log("Connected to database!");
-    })
-    .catch((err)=>{
-        console.log(`Some error occured while connecting to database : ${err}`);
-    })
-}
+    } catch (err) {
+        console.error(`Some error occurred while connecting to database: ${err}`);
+    }
+};
