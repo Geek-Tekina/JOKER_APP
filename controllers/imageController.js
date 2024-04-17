923b9f29c5fcf7c0c04c1c80258544f1e2df0b01
Geek-Tekina <tpbc1717@gmail.com>
2024-04-17 21:52:14 +0530

    Added 40 more Images - Now 50

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -52,6 +52,161 @@ const imageData = [
     _id: "661d4f64d0b5d80b08de6d73",
     url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713196899/r1sdoyelakvvf3folcuf.jpg',
     n_id: 10
+  },
+  {
+    _id: "661d4f64d0b5d80b08de6d73",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713196899/r1sdoyelakvvf3folcuf.jpg',
+    n_id: 10
+  },
+  {
+    _id: "661fed992dbea38f81526ec1",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368469/rjipa2y93kq85ffyuut4.jpg',
+    n_id: 11
+  },
+  {
+    _id: "661fedbb2dbea38f81526ec4",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368503/gemzq9d6c743wzrnwl1c.jpg',
+    n_id: 12
+  },
+  {
+    _id: "661fedf22dbea38f81526ec7",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368558/c0ebzumfwknlsh3nw3jq.jpg',
+    n_id: 13
+  },
+  {
+    _id: "661fee1f2dbea38f81526eca",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368603/hovbq7df6e7hmppu87ls.jpg',
+    n_id: 14
+  },
+  {
+    _id: "661feeb02dbea38f81526ecd",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368748/g1cphrg7rmbq2h6h11bs.jpg',
+    n_id: 15
+  },
+  {
+    _id: "661feec92dbea38f81526ed0",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368773/zzbozhuhmsskvfhuhv6a.jpg',
+    n_id: 16
+  },
+  {
+    _id: "661feedb2dbea38f81526ed3",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368791/fnnrzgg5kknqff4qyqqm.jpg',
+    n_id: 17
+  },
+  {
+    _id: "661feeec2dbea38f81526ed6",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368808/gxxioqmrpse2fi4xnqtv.jpg',
+    n_id: 18
+  },
+  {
+    _id: "661fef042dbea38f81526ed9",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368831/klpxibx7rleseuktbums.jpg',
+    n_id: 19
+  },
+  {
+    _id: "661fef212dbea38f81526edc",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713368861/i8w63qk9btovr4meouis.jpg',
+    n_id: 20
+  },
+  {
+    _id: "661ff0802dbea38f81526edf",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369212/sig5qfohx6htzvleyubl.jpg',
+    n_id: 21
+  },
+  {
+    _id: "661ff0b02dbea38f81526ee2",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369260/upe8fyw4jv21kmimkpde.jpg',
+    n_id: 22
+  },
+  {
+    _id: "661ff0cf2dbea38f81526ee5",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369291/zttm6ttlbjsaffrisjby.jpg',
+    n_id: 23
+  },
+  {
+    _id: "661ff1082dbea38f81526ee8",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369348/cysivbnhvvge5d1ouabb.jpg',
+    n_id: 24
+  },
+  {
+    _id: "661ff1242dbea38f81526eeb",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369377/zsgkkof0yaecuufeaymi.jpg',
+    n_id: 25
+  },
+  {
+    _id: "661ff14d2dbea38f81526eee",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369417/ffzsrtyberaz7wbvndis.jpg',
+    n_id: 26
+  },
+  {
+    _id: "661ff1772dbea38f81526ef1",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369460/j2wtz6kx0ldacrzxwypw.jpg',
+    n_id: 27
+  },
+  {
+    _id: "661ff1a62dbea38f81526ef4",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369506/j3bcuwficztbghfleomi.jpg',
+    n_id: 28
+  },
+  {
+    _id: "661ff1dd2dbea38f81526ef7",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369561/bx1b5rudq0vjrvtt3re8.jpg',
+    n_id: 29
+  },
+  {
+    _id: "661ff23f10cd033c463d305b",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369659/n3d5hrodkp3na4prb5st.jpg',
+    n_id: 30
+  },
+  {
+    _id: "661ff27210cd033c463d305e",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369711/xaw9hw03sj5edlgoerhq.jpg',
+    n_id: 31
+  },
+  {
+    _id: "661ff29310cd033c463d3061",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369743/lpaczgjdyah6iokwskeu.jpg',
+    n_id: 32
+  },
+  {
+    _id: "661ff2b410cd033c463d3064",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369776/sypywe5mzepa4hplynj2.jpg',
+    n_id: 33
+  },
+  {
+    _id: "661ff2e010cd033c463d3067",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369820/wym0y7t5n3luolpmrfxk.jpg',
+    n_id: 34
+  },
+  {
+    _id: "661ff2fc10cd033c463d306a",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369848/nqcnmmxh8b9gjxdacbke.jpg',
+    n_id: 35
+  },
+  {
+    _id: "661ff31510cd033c463d306d",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369873/v3qxkahd1d22ixzzodax.jpg',
+    n_id: 36
+  },
+  {
+    _id: "661ff32d10cd033c463d3070",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369897/yxtahi21asebtpm9aeys.jpg',
+    n_id: 37
+  },
+  {
+    _id: "661ff34510cd033c463d3073",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369921/qxgsbthkecz0j5vxq8zj.jpg',
+    n_id: 38
+  },
+  {
+    _id: "661ff35a10cd033c463d3076",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369942/lkcavne0tvlxtphfypsj.jpg',
+    n_id: 39
+  },
+  {
+    _id: "661ff36c10cd033c463d3079",
+    url: 'https://res.cloudinary.com/dgct4lfji/image/upload/v1713369960/jo0jp6niit07aom0xxis.jpg',
+    n_id: 40
   }
 ]
 // controller for displaying all the images
